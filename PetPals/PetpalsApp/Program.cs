8693dc8f92091a86b1588103346293be8c05dc83
Amulya1609 <133862067+Amulya1609@users.noreply.github.com>
2024-10-17 20:33:26 +0530

    Add files via upload

---
+++ PetPals/PetpalsApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using MainModule;
+
+namespace PetpalsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PetPalsMenu petpalsmenu = new PetPalsMenu();
+            petpalsmenu.Run();
+        }
+    }
+}
