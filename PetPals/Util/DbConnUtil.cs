e89dacea8dd66af81bb5a19307a2cac7d3dc88b8
Amulya1609 <133862067+Amulya1609@users.noreply.github.com>
2024-10-17 20:31:00 +0530

    Add files via upload

---
+++ PetPals/Util/DbConnUtil.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace UtilityProj
+{
+    public static class DBPropertyUtil
+    {
+        private static IConfigurationRoot _configuration;
+        static string s = null;
+        static DBPropertyUtil()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("C:\\Users\\Amulya\\Desktop\\Hexaware\\Assessment\\PetPals\\Util\\appsettings.json",
+               optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
+        }
+        public static string ReturnCn(string key)
+        {
+
+            s = _configuration.GetConnectionString("dbCn");
+
+            return s;
+        }
+    }
+}
